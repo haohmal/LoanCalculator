ed8410510fab60bf544eb04eaa844ca8835df0a9
Andreas Eckerle <a.eckerle@iroka.de>
2021-08-21 00:14:16 +0200

    first working version

--- Loan Calculator/task/creditcalc/creditcalc.py
+++ Loan Calculator/task/creditcalc/creditcalc.py
@@ -1,11 +1,82 @@
 import math
+import argparse
+import sys
+
+arg_parser = argparse.ArgumentParser(description="This program calculates loans.")
+arg_parser.add_argument("--type", choices=["annuity", "diff"], help="Choices are annuity or diff")
+arg_parser.add_argument("--payment", help="The monthly payment amount.")
+arg_parser.add_argument("--principal")
+arg_parser.add_argument("--periods", help="Denotes the number of months needed to repay the loan.")
+arg_parser.add_argument("--interest")
+
+args = arg_parser.parse_args()
+
+if args.type != "diff" and args.type != "annuity":
+    print("Incorrect parameters")
+    sys.exit(-1)
+
+if args.type == "diff" and args.payment is not None:
+    print("Incorrect parameters")
+    sys.exit(-1)
+
+#if len(args) != 5:
+#    print("Incorrect parameters")
+#    sys.exit(-1)
 
 annuity = 0
 principal = 0
 nominal_interest = 0
 interest = 0
 number = 0
+num_params = 0
 
+if args.principal is not None:
+    principal = float(args.principal)
+    if principal <= 0:
+        print("Incorrect parameters")
+        sys.exit(-1)
+    num_params += 1
+else:
+    mode = 'p'
+
+if args.periods is not None:
+    number = int(args.periods)
+    if number <= 0:
+        print("Incorrect parameters")
+        sys.exit(-1)
+    num_params += 1
+else:
+    mode = 'n'
+
+if args.payment is not None:
+    annuity = float(args.payment)
+    if annuity <= 0:
+        print("Incorrect parameters")
+        sys.exit(-1)
+    num_params += 1
+else:
+    mode = 'a'
+
+if args.interest is not None:
+    interest = float(args.interest)
+    if interest <= 0:
+        print("Incorrect parameters")
+        sys.exit(-1)
+    num_params += 1
+else:
+    print("Incorrect parameters")
+    sys.exit(-1)
+
+if args.type == "diff":
+    mode = 'd'
+
+if num_params != 3:
+    print("Incorrect parameters")
+    sys.exit(-1)
+
+def get_diffetentiated_payment(principal, nominal_interest, number, month):
+    part1 = principal * ((month - 1) /number)
+    return math.ceil(principal / number + nominal_interest * (principal - part1))
 
 def get_loan_principal(annuity, nominal_interest, number):
     part1 = nominal_interest * math.pow(1 + nominal_interest, number)
@@ -25,29 +96,16 @@ def get_annuity(principal, nominal_interest, number):
     return math.ceil(principal * nominal_interest * math.pow(1 + nominal_interest, number) / (
                 math.pow(1 + nominal_interest, number) - 1))
 
-
-print("""What do you want to calculate?
-type "n" for number of monthly payments,
-type "a" for annuity monthly payment amount,
-type "p" for loan principal:""")
-mode = input()
-
-if mode != 'p':
-    print("Enter the loan principal:")
-    principal = int(input())
-
-if mode != 'a':
-    print("Enter the annuity payment:")
-    annuity = float(input())
-
-if mode != 'n':
-    print("Enter the number of periods:")
-    number = float(input())
-
-print("Enter the loan interest:")
-interest = float(input())
-
-if mode == 'p':
+if mode == 'd':
+    total_payment = 0
+    for month in range(number):
+        month += 1
+        payment = get_diffetentiated_payment(principal, get_nominal_interest(interest), number, month)
+        total_payment += payment
+        print("Month {0}: payment is {1}".format(month, payment))
+    print()
+    print("Overpayment = {}".format(total_payment - principal))
+elif mode == 'p':
     #print("Your loan principal = 800000!")
     principal = get_loan_principal(annuity, get_nominal_interest(interest), number)
     print("Your loan principal = {}!".format(principal))
@@ -58,4 +116,9 @@ else:
     number = math.ceil(get_number_of_payments(annuity, get_nominal_interest(interest), principal))
     years = number // 12
     months = number % 12
-    print("It will take {0} years and {1} months to repay this loan!".format(years, months))
+    if months > 0:
+        print("It will take {0} years and {1} months to repay this loan!".format(years, months))
+    else:
+        print("It will take {0} years to repay this loan!".format(years))
+    total_payment = number * annuity
+    print("Overpayment = {}".format(total_payment - principal))
